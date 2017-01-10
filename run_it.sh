227bb6571dac1dd03533305e11529f5e19959e4c
Satanette <predark.dante@gmail.com>
2017-01-10 13:30:27 +0200

    Update run_it.sh

--- run_it.sh
+++ run_it.sh
@@ -3,3 +3,11 @@
 /sbin/getcap bytes
 ./bytes $HOSTNAME
 
+
+
+
+##########################################################
+#Sources:
+# https://jan.newmarch.name/go/socket/chapter-socket.html
+# https://linux.die.net/man/7/capabilities
+##########################################################
